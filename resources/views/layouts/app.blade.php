9a79492a4fe9d39e0521987db8c474a11a40b863
Elucinator <shaikhkamranirfan1@gmail.com>
2020-11-24 18:58:11 +0530

    Playing with Blade Template Engine

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Laravel Practice</title>
+    </head>
+    <body>
+        <div class="cotainer">
+            @yield('content')
+        </div>
+        @yield('footer')
+    </body>
+</html>
